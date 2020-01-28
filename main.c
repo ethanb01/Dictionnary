43ef051a94e0415bf98f47303cdfc7c9cc713d62
Ethan Barkate <barkate.ethan@gmail.com>
2020-01-28 19:33:06 +0200

    add comment

--- main.c
+++ main.c
@@ -10,21 +10,21 @@ int main(int argc ,char ** argv){
 	FILE *src = fopen(argv[1],"r");
 	FILE *temp = fopen("temp.txt","w+");
 	char buf[32];
-	if(src==NULL){
+	if(src==NULL)
+	{
 		printf("Could not open FILE %s\n",argv[1]);
 		return -2;
 	}
-	while(fscanf(src, "%s", buf) != EOF)
+	while(fscanf(src, "%s", buf) != EOF) //get word by word the text of the source file 
     {
-        fprintf(temp,"%s ", get_translate(buf,argv[2]));
+        fprintf(temp,"%s ", get_translate(buf,argv[2])); //put in the temp file the translated word(or not) 
     }
-    fclose(src);
     fclose(temp);
 
 	src = fopen(argv[1],"w+");
 	temp = fopen("temp.txt","r");
 	int c;
- 	while( ( c = fgetc(temp) ) != EOF )
+ 	while( ( c = fgetc(temp) ) != EOF ) //copy char by char from the temp to source file
     	fputc(c, src);
     fclose(src);
     fclose(temp);
@@ -41,19 +41,21 @@ int main(int argc ,char ** argv){
 
 }
 
+//function that receive a word and the name of the dictionnary file
+//return the word translated, if ther is no translation the function the original word
 char* get_translate(char* word,char* argv2){
 
 	argv2 ="dic.txt";
 	FILE *dic = fopen(argv2,"r");
-	char *buf  = (char*)malloc(sizeof(char)*32);
+	char *buf  = (char*)malloc(sizeof(char)*32); //potentiel word translated
 	if(dic==NULL){
 		printf("Could not open FILE %s\n",argv2);
 		exit(1);
 	}
-	while(fscanf(dic, "%s", buf) != EOF)
+	while(fscanf(dic, "%s", buf) != EOF) //get the first of the 2 word in the dictionnary line (Hello-Hi, get Hello)
     {
-        if(strcmp(word,buf)==0)
-        	if(fscanf(dic ,"%s", buf))
+        if(strcmp(word,buf)==0) // if it is the word we are looking for
+        	if(fscanf(dic ,"%s", buf)) // put the next word (Hi) in buf
         		return buf;
     }
     fclose(dic);
